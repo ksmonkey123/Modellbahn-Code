efb9707fa04def56806f85105b55ac6c7716f919
Andreas Wälchli <andreas.waelchli@me.com>
2016-04-29 15:26:54 +0200

    light controller rework to include SSI (system state indicator) and button de-bouncing

--- cntr_decoder.X/nbproject/Makefile-default.mk
+++ cntr_decoder.X/nbproject/Makefile-default.mk
@@ -86,7 +86,7 @@ ${OBJECTDIR}/cntr_decoder.o: cntr_decoder.asm  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/cntr_decoder.o.d 
 	@${RM} ${OBJECTDIR}/cntr_decoder.o 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/cntr_decoder.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_SIMULATOR=1 -q -p$(MP_PROCESSOR_OPTION) -u  -l\\\"${OBJECTDIR}/cntr_decoder.lst\\\" -e\\\"${OBJECTDIR}/cntr_decoder.err\\\" $(ASM_OPTIONS)   -o\\\"${OBJECTDIR}/cntr_decoder.o\\\" \\\"cntr_decoder.asm\\\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/cntr_decoder.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION) -u  -l\\\"${OBJECTDIR}/cntr_decoder.lst\\\" -e\\\"${OBJECTDIR}/cntr_decoder.err\\\" $(ASM_OPTIONS)   -o\\\"${OBJECTDIR}/cntr_decoder.o\\\" \\\"cntr_decoder.asm\\\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/cntr_decoder.o"
 	@${FIXDEPS} "${OBJECTDIR}/cntr_decoder.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
@@ -106,7 +106,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/cntr_decoder.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ../moba_libraries.X/dist/default/debug/moba_libraries.X.lib  
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_SIMULATOR=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/cntr_decoder.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}   ../moba_libraries.X/dist/default/debug/moba_libraries.X.lib  
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PK3=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/cntr_decoder.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}   ../moba_libraries.X/dist/default/debug/moba_libraries.X.lib  
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/cntr_decoder.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ../moba_libraries.X/dist/default/production/moba_libraries.X.lib 
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
